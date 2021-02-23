20c320817d20b44249aa433eb38557993fd00e16
Gabriel110 <gabrielcorreiadev@gmail.com>
2021-02-23 12:55:24 -0300

    configurando next.js

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+
+import { ExperienceBar } from '../componentes/ExperienceBar';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="container">
+
+      <ExperienceBar />
+    </div>
+  )
+}
